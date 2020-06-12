326a15bcc7ffbddbd42fcf19042adc93736a4ef2
bar.wachtel <bar.wachtel@myob.com>
2020-06-12 13:53:51 +1000

    Change into basic websocket server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+
+const WebSocket = require('ws');
 
 const port = process.env.PORT || 1337;
-server.listen(port);
 
+const wss = new WebSocket.Server({ port: port });
 console.log("Server running at http://localhost:%d", port);
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('Hello Client! from Server <3');
+});
+
